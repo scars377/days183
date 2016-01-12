5a158b00b76cc4406cdd5d351c449444eef00f60
scars <scars@medialand.tw>
2016-01-12 18:47:53 +0800

    styles changed 將清空記錄按鈕移到記錄下方 修改了小螢幕上的樣式 修改 App:removeAllRecords 以移除網址 hash

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,10 +5,7 @@ var Inputs = require('Inputs.jsx');
 var Records = require('Records.jsx');
 var serializer = require('RecordsSerializer.js')
 
-
 console.clear();
-
-
 
 /*
  █████  ██████  ██████
@@ -19,15 +16,8 @@ console.clear();
 */
 var App = React.createClass({
 	getInitialState: function() {
-		return {
-			records:[],
-			hash:''
-		};
+		return {records: [], hash: ''};
 	},
-
-
-
-
 
 	/*
 	██████  ███████  ██████  ██████  ██████  ██████  ███████
@@ -36,47 +26,36 @@ var App = React.createClass({
 	██   ██ ██      ██      ██    ██ ██   ██ ██   ██      ██
 	██   ██ ███████  ██████  ██████  ██   ██ ██████  ███████
 	*/
-	addRecord:function(r){
+	addRecord: function(r) {
 		var rr = this.state.records;
 		rr.push(r);
 		this.setRecords(rr);
 	},
-	removeRecord:function(idx){
+	removeRecord: function(idx) {
 		var rr = this.state.records;
-		rr.splice(idx,1)
+		rr.splice(idx, 1)
 		this.setRecords(rr)
 	},
-    removeAllRecord:function(){
-        if(confirm('是否清空紀錄') == true){
-          this.setState({
-			records: [],
-			hash: ''
-		  });
-        }
-    },
-	toggleRecord:function(idx){
+	removeAllRecord: function(e) {
+		if (e.ctrlKey || confirm('是否清空紀錄') == true) {
+			location.hash='';
+			this.setRecords([]);
+		}
+	},
+	toggleRecord: function(idx) {
 		var rr = this.state.records;
 		rr[idx].dep = !rr[idx].dep;
 		this.setRecords(rr)
 	},
 
-
-	setRecords:function(rr){
-		rr = rr.sort(function(r1,r2){
-			return r1.date.getTime()-r2.date.getTime()
+	setRecords: function(rr) {
+		rr = rr.sort(function(r1, r2) {
+			return r1.date.getTime() - r2.date.getTime()
 		});
 		var hash = serializer.stringify(rr);
-		window.location.hash=hash;
-		this.setState({
-			records: rr,
-			hash: hash
-		})
+		window.location.hash = hash;
+		this.setState({records: rr, hash: hash})
 	},
-
-
-
-
-
 
 	/*
 	 ██████ ██████  ███    ███
@@ -86,7 +65,7 @@ var App = React.createClass({
 	 ██████ ██████  ██      ██
 	*/
 	componentDidMount: function() {
-		var hash = window.location.hash.replace(/^#/,'');
+		var hash = window.location.hash.replace(/^#/, '');
 		var rr = serializer.parse(hash);
 		this.setRecords(rr);
 	},
@@ -98,9 +77,9 @@ var App = React.createClass({
 	██   ██ ██      ██  ██ ██ ██   ██ ██      ██   ██
 	██   ██ ███████ ██   ████ ██████  ███████ ██   ██
 	*/
-	render:function(){
-		var hashURL = window.location.origin+window.location.pathname+'#'+this.state.hash;
-		return(
+	render: function() {
+		var hashURL = window.location.origin + window.location.pathname + '#' + this.state.hash;
+		return (
 			<div className='app'>
 				<h1>最近一年居住國內超過183日試算器</h1>
 				<div className='title-info'>
@@ -114,24 +93,24 @@ var App = React.createClass({
 				<Inputs setRecords={this.setRecords} addRecord={this.addRecord} removeAllRecord={this.removeAllRecord}/>
 				日期格式: 1030602<br/>
 
-
-				<Records
-					records={this.state.records}
-					removeRecord={this.removeRecord}
-					toggleRecord={this.toggleRecord}
-					/>
+				<Records records={this.state.records} removeRecord={this.removeRecord} toggleRecord={this.toggleRecord}/>
 
 				<br/>
 
-				{this.state.hash==''?null:
-					<div>
-						記錄網址: <a href={hashURL}>{hashURL}</a>
+				<button className='remove-all' onClick={this.removeAllRecord}>
+					清空紀錄
+				</button>
+
+				{this.state.hash == ''?null:
+					<div className='record-url'>
+						<span className='caption'>記錄網址</span>{': '}
+						<a href={hashURL}>{hashURL}</a>
 					</div>
 				}
 				<br/>
 				<br/>
 				<div className='tips'>
-                  <h2>操作說明</h2><br/>
+					<h2>操作說明</h2><br/>
 					<p>
 						輸入框中<br/>
 						按 Q 或 Numpad+ 可切換為 [入境]<br/>
@@ -140,24 +119,27 @@ var App = React.createClass({
 					</p>
 					<p>
 						記錄的出入境標記可點擊以切換<br/>
-						Ctrl-點擊記錄的移除標記可略過確認框
+						Ctrl-點擊記錄的移除標記可略過確認框<br/>
+						Ctrl-點擊「清空記錄」按鈕可略過確認框
 					</p>
-                    <br/>
+					<br/>
 
-                  <h2>一、移民署格式匯入（承辦作業專用）</h2>
+					<h2>一、移民署格式匯入（承辦作業專用）</h2>
 
-                    <p>1.已取得內政部移民署線上應用服務系統帳號，並有入出境資訊查詢權限者，可透過該系統查詢歷次出入境紀錄。</p>
-                    <p>2.將該系統產出之歷次出入境紀錄，整批複製並貼上到183日試算系統欄位，即可運算是否符合最近一年居住國內超過183日之規定。</p>
-                    <br/>
+					<p>1.已取得內政部移民署線上應用服務系統帳號，並有入出境資訊查詢權限者，可透過該系統查詢歷次出入境紀錄。</p>
+					<p>2.將該系統產出之歷次出入境紀錄，整批複製並貼上到183日試算系統欄位，即可運算是否符合最近一年居住國內超過183日之規定。</p>
+					<br/>
 
-                  <h2>二、手動輸入</h2>
+					<h2>二、手動輸入</h2>
 
-                    <p>1.點選「出境」或「入境」後，輸入對應之出境或入境日期，再點選「加入」。</p>
-                    <p>2.日期格式範例：如為105年1月1日，則輸入「1050101」</p>
-                    <p>3.將歷次出入境紀錄依序輸入完畢後，即可運算是否符合最近一年居住國內超過183日之規定。</p>
+					<p>1.點選「出境」或「入境」後，輸入對應之出境或入境日期，再點選「加入」。</p>
+					<p>2.日期格式範例：如為105年1月1日，則輸入「1050101」</p>
+					<p>3.將歷次出入境紀錄依序輸入完畢後，即可運算是否符合最近一年居住國內超過183日之規定。</p>
 				</div>
 				<br/>
-				<a href="https://github.com/scars377/days183" style={{fontSize:'12px'}} target="_blank">
+				<a href="https://github.com/scars377/days183" style={{
+					fontSize: '12px'
+				}} target="_blank">
 					https://github.com/scars377/days183
 				</a>
 			</div>
@@ -166,4 +148,4 @@ var App = React.createClass({
 });
 
 module.exports = App;
-ReactDOM.render(<App/>,$('#wrapper')[0])
+ReactDOM.render( < App />, $('#wrapper')[0])
--- src/js/Inputs.jsx
+++ src/js/Inputs.jsx
@@ -1,94 +1,89 @@
 require('../css/Inputs.scss');
 
-
-
-
-
 var Inputs = React.createClass({
 	getInitialState: function() {
-		return {
-			dep: false,
-			input: '',
-			enabled:false,
-		};
+		return {dep: false, input: '', enabled: false};
 	},
 
-	getDateFromString: function(s){
+	getDateFromString: function(s) {
 
-		var m=s.match(/^(\d{2,3})(\d{2})(\d{2})$/);
-		if(!m) return null;
+		var m = s.match(/^(\d{2,3})(\d{2})(\d{2})$/);
+		if (!m)
+			return null;
 
-		var d = new Date((parseInt(m[1],10)+1911)+'-'+m[2]+'-'+m[3])
-		if(isNaN(d.getTime())) return null;
+		var d = new Date((parseInt(m[1], 10) + 1911) + '-' + m[2] + '-' + m[3])
+		if (isNaN(d.getTime()))
+			return null;
 
 		var y = d.getFullYear();
-		if(y < 2000 || y > 2063) return null;
+		if (y < 2000 || y > 2063)
+			return null;
 
 		return d
 	},
 
-	getDateFromString2: function(s){
+	getDateFromString2: function(s) {
 
-		var m=s.match(/^(\d{4})(\d{2})(\d{2})$/);
-		if(!m) return null;
+		var m = s.match(/^(\d{4})(\d{2})(\d{2})$/);
+		if (!m)
+			return null;
 
-		var d = new Date((parseInt(m[1],10))+'/'+m[2]+'/'+m[3])
-		if(isNaN(d.getTime())) return null;
+		var d = new Date((parseInt(m[1], 10)) + '/' + m[2] + '/' + m[3])
+		if (isNaN(d.getTime()))
+			return null;
 
 		var y = d.getFullYear();
-		if(y < 2000 || y > 2063) return null;
+		if (y < 2000 || y > 2063)
+			return null;
 
 		return d
 	},
 
-
-	setDep:function(dep){
-		return function(){
-			this.setState({dep:dep})
+	setDep: function(dep) {
+		return function() {
+			this.setState({dep: dep})
 		}.bind(this)
 	},
-	inputChange:function(e){
-		var s = e.target.value.replace(/\D/g,'').substr(0,7)
-		var enabled = this.getDateFromString(s)!=null;
-		this.setState({
-			input:s,
-			enabled:enabled,
-		})
+	inputChange: function(e) {
+		var s = e.target.value.replace(/\D/g, '').substr(0, 7)
+		var enabled = this.getDateFromString(s) != null;
+		this.setState({input: s, enabled: enabled})
 	},
-	inputDown:function(e){
-		switch(e.keyCode){
-			case 13://enter
-				if(this.state.enabled) this.addRecord()
+	inputDown: function(e) {
+		switch (e.keyCode) {
+			case 13: //enter
+				if (this.state.enabled)
+					this.addRecord()
 				break;
 			case 81: // Q
 			case 109: //-
-				this.setState({dep:true});
+				this.setState({dep: true});
 				break;
 			case 87: // W
 			case 107: //+
-				this.setState({dep:false});
+				this.setState({dep: false});
 				break;
-			case 69:  // E
-			case 111:  // /
-				this.setState({dep:!this.state.dep})
+			case 69: // E
+			case 111: // /
+				this.setState({dep: !this.state.dep});
 				break;
 
 			default:
 				// console.log(e.keyCode);
 		}
 	},
-	addRecord:function(){
+	addRecord: function() {
 		this.props.addRecord({
-			date:this.getDateFromString(this.state.input),
-			dep: this.state.dep,
+			date: this.getDateFromString(this.state.input),
+			dep: this.state.dep
 		})
 		this.setState({
-			input:'',
-			enabled:false,
+			input: '',
+			enabled: false,
 			dep: !this.state.dep
 		})
 	},
-	importFromText:function(){
+	importFromText: function() {
 
 		var val = React.findDOMNode(this.refs.import).value;
 
@@ -96,23 +91,22 @@ var Inputs = React.createClass({
 		var records = [];
 		var that = this;
 
-
-		lines.forEach(function(line){
-			if(line){
+		lines.forEach(function(line) {
+			if (line) {
 				var tokens = line.split(/[ \t]+/);
 
-				if(tokens[1] == "出境" || tokens[1] =="入境"){
+				if (tokens[1] == "出境" || tokens[1] == "入境") {
 					var dep = null;
 
-					if(tokens[1] == "出境"){
+					if (tokens[1] == "出境") {
 						dep = true;
-					}else if(tokens[1] == "入境"){
+					} else if (tokens[1] == "入境") {
 						dep = false;
 					}
 
-					if(dep != null ){
+					if (dep != null) {
 						records.push({
-							date:that.getDateFromString2(tokens[2]),
+							date: that.getDateFromString2(tokens[2]),
 							dep: dep
 						});
 						debugger;
@@ -122,64 +116,43 @@ var Inputs = React.createClass({
 			}
 		});
 
-
 		this.props.setRecords(records);
 	},
 	componentDidMount: function() {
 		ReactDOM.findDOMNode(this.refs.input).focus();
 	},
-	render:function(){
-		return(
+	render: function() {
+		return (
 			<div>
 				<h2>移民署格式匯入（承辦作業專用）</h2>
-				<textarea
-					className='paste-input'
-					ref = "import"
-					onChange = {this.importFromText}
-					/>
+				<textarea className='paste-input' ref="import" onChange= {this.importFromText}/>
 
-
-				<hr />
+				<hr/>
 
 				<h2>手動輸入</h2>
 				<div className='inputs'>
 
-					<button
-						className={'dep '+( this.state.dep?'active':'deactive')}
-						onClick={this.setDep(true)}
-						>
+					<button className={'dep ' + (this.state.dep
+						? 'active'
+						: 'deactive')} onClick={this.setDep(true)}>
 						出境
 					</button>
 
-					<button
-						className={'arr '+(!this.state.dep?'active':'deactive')}
-						onClick={this.setDep(false)}
-						>
+					<button className={'arr ' + (!this.state.dep
+						? 'active'
+						: 'deactive')} onClick={this.setDep(false)}>
 						入境
 					</button>
 
-					<input
-						type='text'
-						ref='input'
-						placeholder='輸入日期'
-						value={this.state.input}
-						onKeyDown={this.inputDown}
-						onChange={this.inputChange}/>
+					<input type='text' ref='input' placeholder='輸入日期' value={this.state.input} onKeyDown={this.inputDown} onChange={this.inputChange}/>
 
-					<button
-						className={this.state.enabled?'enabled':'disabled'}
-						onClick={this.addRecord}
-						>
+					<button className={this.state.enabled
+						? 'enabled'
+						: 'disabled'} onClick={this.addRecord}>
 						加入
 					</button>
-                    <button
-						className='enabled'
-						onClick={this.props.removeAllRecord}
-						>
-						清空紀錄
-					</button>
 				</div>
-				<hr />
+				<hr/>
 			</div>
 		);
 	}
